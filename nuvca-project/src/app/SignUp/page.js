1881903b6784c5c3ff045096cdac3559eb42178d
Codngwthubaid <codngwthubaid@gmail.com>
2024-08-21 15:37:34 +0530

    Trying to solved the issue of SingUp page and till now it not solved

--- nuvca-project/src/app/SignUp/page.js
+++ nuvca-project/src/app/SignUp/page.js
@@ -13,25 +13,28 @@ const SignUpPage = () => {
         password: ""
     })
 
-    // const [buttonDisable, setButtonDisable] = useState(false)
     const signUp = async () => {
         try {
             const response = await axios.post("/api/users/SignUpRoute", user)
             console.log("SignUp Successfull", response.data);
-            router.push("/Login")
-
+            if (response.status == 100) {
+            setInterval(() => {
+                console.log('Before');
+                router.push("/Login")
+                console.log('After');
+            }, 100);
+        } else{
+            console.log('Unwanted Error Comes !!!');   
+        }
         } catch (error) {
             console.log("SignUp Failed !!!");
         }
     }
-    // useEffect(() => {
-    //     if (user.userName.length > 0 && user.email.length > 0 && user.tel.length > 0 && user.password.length > 0) {
-    //         setButtonDisable(false)
-    //     }
-    //     else {
-    //         setButtonDisable(true)
-    //     }
-    // }, [user])
+
+    const handlerSignUp = (e) => {
+        e.preventDefault()
+        signUp()
+    }
 
 
     return (
@@ -63,7 +66,7 @@ const SignUpPage = () => {
                     </div>
                     <div className="mb-5">
                         <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mobile Number</label>
-                        <input onChange={(e) => setUser({ ...setUser, tel: e.target.value })}
+                        <input onChange={(e) => setUser({ ...user, tel: e.target.value })}
                             value={user.tel}
                             name="tel"
                             type="tel"
@@ -84,13 +87,13 @@ const SignUpPage = () => {
                     <div className="flex flex-col gap-y-2 sm:flex-row sm:justify-between gap-x-5">
                         <button
                             type="submit"
-                            onClick={signUp}
+                            onClick={handlerSignUp}
                             className="bg-blue-700 hover:bg-blue-800 focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-white">Sign Up
                         </button>
 
-                        <button 
-                        type="submit" 
-                        className="bg-blue-700 hover:bg-blue-800 focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-white  flex justify-center items-center gap-x-1">
+                        <button
+                            type="submit"
+                            className="bg-blue-700 hover:bg-blue-800 focus:ring-4 rounded-lg text-sm px-5 py-2.5 text-white  flex justify-center items-center gap-x-1">
                             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
                                 <path fillRule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clipRule="evenodd" />
                             </svg>
