5ea648d26245051230b3f522352cfcce28d3adef
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-06 13:27:44 +0530

    Currently working on the responsiveness of main page

--- src/app/page.js
+++ src/app/page.js
@@ -106,48 +106,13 @@ const page = () => {
                 </div>
               </section>
               <section className='sm:w-2/5 flex justify-center items-center'>
-                <Carousel className="w-full max-w-xs">
-                  <CarouselContent>
-                    {Array.from({ length: 5 }).map((_, index) => (
-                      <CarouselItem key={index}>
-                        <div className="">
-                          <Card>
-                            {/* <CardContent className="flex aspect-square items-center justify-center p-6">
-                              <span className="text-4xl font-semibold">{index + 1}</span>
-                            </CardContent> */}
-                            <CardContent>
-                              <div>
-                                <Image
-                                  src="/img/ImgFirstMain.jpg"
-                                  width={500}
-                                  height={500}
-                                  alt="Picture of the author"
-                                />
-                              </div>
-                            </CardContent>
-                          </Card>
-                          <Card>
-                            {/* <CardContent className="flex aspect-square items-center justify-center p-6">
-                              <span className="text-4xl font-semibold">{index + 1}</span>
-                            </CardContent> */}
-                            <CardContent>
-                              <div>
-                                <Image
-                                  src="/img/ImgSecondMain.jpg"
-                                  width={500}
-                                  height={500}
-                                  alt="Picture of the author"
-                                />
-                              </div>
-                            </CardContent>
-                          </Card>
-                        </div>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                  <CarouselPrevious />
-                  <CarouselNext />
-                </Carousel>
+                <Image
+                  className="rounded-3xl"
+                  src="/img/MainImg.webp"
+                  width={400}
+                  height={400}
+                  alt="Picture of the author"
+                />
               </section>
             </main>
           </div>
