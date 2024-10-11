e715f5b89801489066ee30111d2e78b770a0aea7
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-11 18:59:57 +0530

    Do changing in navbar height

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <>
             <header>
-                <nav className="bg-white border-gray-200 dark:bg-gray-900 h-auto sm:h-[12vh] fixed top-0 left-0 z-20 w-full ">
+                <nav className="bg-white border-gray-200 dark:bg-gray-900 h-auto sm:h-[10vh] fixed top-0 left-0 z-20 w-full ">
                     <div className="flex flex-wrap justify-between items-center min-w-full max-w-screen-xl p-4">
                         <Link href="/pages/Features">
                         <div className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"><span className='text-slate-600'>&#60;</span><span className="font-bold text-[#121b30]">NU-</span><span className="font-bold text-blue-500">VCA</span><span className='text-slate-600'>&#47;&#62;</span></div>
