9649cacbed1e601c144281c5af2c5f39177020f2
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-20 12:34:11 +0530

    Creates some new folders

---
+++ src/app/pages/Contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Im Contact page
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/pages/Licensing/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Im Licensing page
+    </div>
+  )
+}
+
+export default page
