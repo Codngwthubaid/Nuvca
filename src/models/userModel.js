8e4a58729a7e7a38448bfc6ca82432612f448e85
Codngwthubaid <codngwthubaid@gmail.com>
2025-02-01 08:56:51 +0530

    Modified the model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,14 +1,3 @@
-// Old Code
-// import mongoose from 'mongoose';
-// const userSchema = new mongoose.Schema({
-//     username: { type: String, require: true },
-//     email: { type: String, require: true, unique: true },
-//     password: { type: String, select: false },
-//     GoogleID: { type: String, require: true }
-// })
-// export const User = mongoose.models.User || mongoose.model('User', userSchema);
-
-// New Code 
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     userName: {
