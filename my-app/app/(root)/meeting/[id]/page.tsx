77936bcf64bfff9437d11eb62d8e8cfe784a3281
Codngwthubaid <codngwthubaid@gmail.com>
2025-03-07 15:55:07 +0530

    Create custom hook : useGetCallById

--- my-app/app/(root)/meeting/[id]/page.tsx
+++ my-app/app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+"use client"
+import Loader from '@/components/Loader'
+import MeetingRoom from '@/components/MeetingRoom'
+import MeetingSetup from '@/components/MeetingSetup'
+import { useGetCallById } from '@/hooks/useGetCallById'
+import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
+import React, { useState } from 'react'
 
-const Meeting = ({ params }: { params: Promise<{ id: string }>}) => {
+const Meeting = ({ params : {id}}: { params: { id: string }}) => {
+
+  const {user, isLoaded} = useUser()
+  const [isSetupCompleted, setIsSetupCompleted] = useState(false);
+  const {call, isCallLoading} = useGetCallById(id)
+
+  if(!call || isCallLoading) return <Loader />
+  
   return (
-    <div>Meeting</div>
+    <main className='h-screen w-full'>
+      <StreamCall call={call}>
+        <StreamTheme>
+          {isSetupCompleted ? (
+            <MeetingSetup/>
+          ) : (
+            <MeetingRoom/>
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
   )
 }
 
