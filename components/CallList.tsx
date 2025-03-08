6b8ce94aaa5c5e2a7f2aa9b43baa877b9df02f0d
Codngwthubaid <codngwthubaid@gmail.com>
2025-03-08 18:18:58 +0530

    init cards for upcoming, previous, recoding calls presentation

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { useGetCalls } from '@/hooks/useGetCalls'
 import { CallRecording } from '@stream-io/node-sdk'
+import { Call } from '@stream-io/video-react-sdk';
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
+import MeetingCard from './MeetingCard';
+import Loader from './Loader';
 
 const CallList = ({ type }: { type: "upcoming" | "recordings" | "ended" }) => {
 
@@ -38,12 +41,32 @@ const CallList = ({ type }: { type: "upcoming" | "recordings" | "ended" }) => {
     const calls = getCalls();
     const noCallsMessage = getNoCallsMessage();
 
+    if(isLoading) return <Loader />
 
     return (
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
-
-
-
+            {calls && calls.length > 0 ? calls.map((meeting: Call | CallRecording) => (
+                <MeetingCard
+                    key={(meeting as Call).id}
+                    icon={
+                        type === "ended"
+                            ? "/icons/previous.svg"
+                            : type === "upcoming"
+                                ? "/icons/upcoming.svg"
+                                : "/icons/recordings.svg"
+                    }
+                    title={(meeting as Call).state.custom?.desc}
+                    date={(meeting as Call).state.startsAt?.toLocaleString() || "N/A"} 
+                    isPreviousMeeting={}
+                    buttonIcon1={}
+                    handleClick={}
+                    link={}
+                    buttonText={}
+                />
+            )) : (
+                "No Calls"
+            )
+            }
         </div>
     )
 }
--- constants/index.ts
+++ constants/index.ts
@@ -27,3 +27,10 @@ export const sidebarLinks = [
 ]
 
 
+export const avatarImages = [
+    "/icons/avatar-1.svg",
+    "/icons/avatar-2.svg",
+    "/icons/avatar-3.svg",
+    "/icons/avatar-4.svg",
+    "/icons/avatar-5.svg"
+]
