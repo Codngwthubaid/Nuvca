02ddd2bc48074652afcece352718f0e63ee975d9
Codngwthubaid <codngwthubaid@gmail.com>
2025-03-09 07:05:57 +0530

    add useEffect to handle asycn call

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,13 +1,53 @@
+// "use client";
+// import Loader from "@/components/Loader";
+// import MeetingRoom from "@/components/MeetingRoom";
+// import MeetingSetup from "@/components/MeetingSetup";
+// import { useGetCallById } from "@/hooks/useGetCallById";
+// import { useUser } from "@clerk/nextjs";
+// import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+// import React, { useState } from "react";
+
+// interface MeetingPageProps {
+//   params: {
+//     id: string;
+//   };
+// }
+
+// const Meeting = ({ params }: MeetingPageProps) => {
+//   const { id } = params; 
+//   const { isLoaded } = useUser();
+//   const [isSetupCompleted, setIsSetupCompleted] = useState(false);
+
+//   const { call, isCallLoading } = useGetCallById(id);
+
+//   if (!isLoaded || !call || isCallLoading) return <Loader />;
+
+//   return (
+//     <main className="h-screen w-full">
+//       <StreamCall call={call}>
+//         <StreamTheme>
+//           {!isSetupCompleted ? (
+//             <MeetingSetup setIsSetupCompleted={setIsSetupCompleted} />
+//           ) : (
+//             <MeetingRoom />
+//           )}
+//         </StreamTheme>
+//       </StreamCall>
+//     </main>
+//   );
+// };
+
+// export default Meeting;
+
 "use client";
 import Loader from "@/components/Loader";
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
 import { useGetCallById } from "@/hooks/useGetCallById";
 import { useUser } from "@clerk/nextjs";
-import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
-import React, { useState } from "react";
+import { Call, StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React, { useState, useEffect } from "react";
 
-// Define the props type for the page component
 interface MeetingPageProps {
   params: {
     id: string;
@@ -15,11 +55,26 @@ interface MeetingPageProps {
 }
 
 const Meeting = ({ params }: MeetingPageProps) => {
-  const { id } = params; 
+  const { id } = params;
   const { isLoaded } = useUser();
   const [isSetupCompleted, setIsSetupCompleted] = useState(false);
+  const [call, setCall] = useState<Call | undefined>(undefined);
+  const [isCallLoading, setIsCallLoading] = useState(true);
 
-  const { call, isCallLoading } = useGetCallById(id);
+  useEffect(() => {
+    const fetchCall = async () => {
+      try {
+        const { call, isCallLoading } = await useGetCallById(id);
+        setCall(call);
+        setIsCallLoading(isCallLoading);
+      } catch (error) {
+        console.error("Error fetching call:", error);
+        setIsCallLoading(false);
+      }
+    };
+
+    fetchCall();
+  }, [id]);
 
   if (!isLoaded || !call || isCallLoading) return <Loader />;
 
